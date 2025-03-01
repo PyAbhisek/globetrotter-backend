0114f9d17ae8a640a3cd03d970dcc6fd4bdec66c
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 01:48:14 +0530

    sending options to choose from

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -3,23 +3,45 @@ const Destination = require("../models/destination");
 let lastIndex = -1;
 
 const getDestination = async (req, res) => {
-  try {
-    const count = await Destination.countDocuments();
-    if (count === 0) {
-      return res.status(404).json({ message: "No destinations found" });
+    try {
+      const count = await Destination.countDocuments();
+      if (count === 0) {
+        return res.status(404).json({ message: "No destinations found" });
+      }
+  
+      // Get the next destination in sequence
+      lastIndex = (lastIndex + 1) % count;
+      const correctDestination = await Destination.findOne()
+        .skip(lastIndex)
+        .select("alias clues name -_id");
+  
+      if (!correctDestination) {
+        return res.status(404).json({ message: "No destinations found" });
+      }
+  
+      // Get 3 incorrect destinations
+      const incorrectDestinations = await Destination.aggregate([
+        { $match: { name: { $ne: correctDestination.name } } },
+        { $sample: { size: 3 } }, // Randomly pick 3 wrong answers
+        { $project: { name: 1, _id: 0 } }
+      ]);
+  
+      // Combine correct and incorrect answers
+      let options = [correctDestination.name, ...incorrectDestinations.map(dest => dest.name)]
+  
+      // Shuffle the options randomly
+      options.sort(() => Math.random() - 0.5);
+  
+      res.json({
+        alias: correctDestination.alias,
+        clues: correctDestination.clues,
+        options
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Server Error", error: error.message });
     }
+  };
 
-    // Get the next destination in sequence
-    lastIndex = (lastIndex + 1) % count;
-    const destination = await Destination.findOne()
-      .skip(lastIndex)
-      .select("alias clues -_id");
-
-    res.json(destination);
-  } catch (error) {
-    res.status(500).json({ message: "Server Error", error: error.message });
-  }
-};
 
 const checkDestination = async (req, res) => {
   try {
