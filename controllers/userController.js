0a35fb21be7374d1d61aea173de07c521c629d3d
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 01:34:27 +0530

    updated the input for UUID

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
 const User = require("../models/User");
-const { v4: uuidv4 } = require("uuid");
 
 const registerUser = async (req, res) => {
-  const { username } = req.body;
+  const { username,uniqueID } = req.body;
   try {
     const existingUser = await User.findOne({ username });
-    if (existingUser) return res.json({ success: true, uuid: existingUser.uuid });
+    if (existingUser) return res.json({ success: false, uuid: existingUser.uuid });
 
-    const newUser = new User({ uuid: uuidv4(), username });
+    const newUser = new User({ uuid: uniqueID, username });
     await newUser.save();
     res.json({ success: true, uuid: newUser.uuid });
   } catch (err) {
-    res.status(500).json({ success: false, message: "Server error" });
+
+    res.status(500).json({ success: false, message: `Server error ${err}` });
   }
 };
 
