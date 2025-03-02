1a6116621dc5b30f07f3db5ce7bf178f593b518c
PyAbhisek <139870038+PyAbhisek@users.noreply.github.com>
2025-03-02 09:56:55 +0530

    username in usermodel is required now

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   uuid: { type: String, required: true, unique: true },
-  username: { type: String, unique: true, sparse: true },
+  username: { type: String, unique: true, sparse: true,required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
